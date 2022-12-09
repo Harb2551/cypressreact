dc3c9232baf39893dcc2602d8ec0924ad17b38ac
Harshit Bhargava <hb@HARSHITs-MacBook-Air.local>
2022-12-09 16:53:26 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useEffect, useRef, useState} from "react";
 
 function App() {
+
+  var [text,setText] = useState('');
+  var count = useRef(0);
+
+  useEffect(()=>{
+    count.current = count.current + 1;
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><b>UseRef in React</b></h1>
+      <input type ="text" placeholder="Enter Text" value={text} onChange={(e)=>setText(e.target.value)}/>
+      <h3 > This page is rendered {count.current} times </h3>
     </div>
   );
 }
